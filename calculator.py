3027a34fa0cea1d3d3ce72e4f926bdba810b97fe
Mészáros Imre <meszaros.imre@diak.szbi-pg.hu>
2024-10-02 11:28:18 +0200

    Calculator

---
+++ calculator.py
@@ -0,0 +1,26 @@
+def calcSum(a,b):
+    return a + b
+
+def calcSub(a,b):
+    return a - b
+
+def calcMult(a,b):
+    return a * b
+
+def calcDiv(a,b):
+    return a / b
+
+num1 = int(input())
+op = input()
+num2 = int(input())
+a = ""
+if op == '+':
+    a = calcSum(num1,num2)
+elif op == '-':
+    a = calcSub(num1,num2)
+elif op == '*':
+    a = calcMult(num1,num2)
+elif op == '/':
+    a = calcDiv(num1,num2)
+
+print(f"= {a}")
