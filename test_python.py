072d8cd2f60348ba3c6834a0f8dcf670bd36ba2d
Mészáros Imre <meszaros.imre@diak.szbi-pg.hu>
2024-10-09 11:33:58 +0200

    calcMult and calcDiv added to test_python and tested

--- test_python.py
+++ test_python.py
@@ -12,3 +12,15 @@ def test_sub():
     assert calcSub(-1,2) == -3
     assert calcSub(-1,-2) == 1
     assert calcSub(0,0) == 0
+
+def test_mult():
+    assert calcMult(1,4) == 4
+    assert calcMult(-3,-5) == 15
+    assert calcMult(4,-2) == -8
+    assert calcMult(0,0) == 0
+
+def test_div():
+    assert calcDiv(3,1) == 3
+    assert calcDiv(-6,2) == -3
+    assert calcDiv(-12,-3) == 4
+    assert calcDiv(0,1) == 0
